c159b3616ef74fa2d3cda8ad517de414bf11d429
LuisOT25 <delatorreluis81@gmail.com>
2023-02-19 21:05:52 -0600

    Creacion de las clases Autenticador y Token que sriven para generar un token de autorizacion

--- src/main/java/com/APIrest/app/dtos/Token.java
+++ src/main/java/com/APIrest/app/dtos/Token.java
@@ -1,5 +1,50 @@
 package com.APIrest.app.dtos;
 
-public class Token {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+public class Token{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Token.class);
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    private Instant momentoExpiracion;
+
+    public static Token registrarToken(Token token) {
+        LOGGER.trace("registrarToken({})", token.tokenType);
+        Token nuevo = new Token();
+        nuevo.accessToken = token.getAccessToken();
+        nuevo.tokenType = token.getTokenType();
+        nuevo.expiresIn = token.expiresIn;
+        nuevo.calcularMomentoExpiracion();
+        return nuevo;
+    }
+
+    public void calcularMomentoExpiracion() {
+        if (this.expiresIn == 0) this.expiresIn = 3600;
+        this.momentoExpiracion = Instant.now()
+                .plusSeconds(this.expiresIn)
+                .minusSeconds(60);
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public boolean isExpired() {
+        return this.momentoExpiracion.isAfter(Instant.now());
+    }
 
 }
---
+++ src/main/java/com/APIrest/app/services/AutenticadorSpotify.java
@@ -0,0 +1,55 @@
+package com.APIrest.app.services;
+
+import com.APIrest.app.dtos.Token;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import java.util.Base64;
+import java.util.Objects;
+
+@Component
+public class AutenticadorSpotify {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutenticadorSpotify.class);
+
+    @Value("${spotify.auth_url:https://accounts.spotify.com/api/token}")
+    String authUrl;
+
+    @Value("${spotify.client_id}")
+    String clientId;
+
+    @Value("${spotify.client_secret}")
+    String clientSecret;
+
+    private Token authToken;
+    public Token getAccessToken() {
+        LOGGER.debug("getAccessToken()");
+        if (this.authToken== null || this.authToken.isExpired()) {
+            this.authToken = obtenerNuevoAccessToken();
+        }
+        return authToken;
+    }
+
+    private Token obtenerNuevoAccessToken(){
+        RestTemplate restTemplate = new RestTemplate();
+        String b64Codes = Base64
+                .getEncoder()
+                .encodeToString((clientId + ":" + clientSecret).getBytes());
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "client_credentials");
+        RequestEntity<Object> request = RequestEntity
+                .method(HttpMethod.POST,authUrl)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + b64Codes)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .body(body);
+
+        ResponseEntity<Token> response =restTemplate.postForEntity(authUrl,request, Token.class);
+        return Token.registrarToken(Objects.requireNonNull(response.getBody()));
+    }
+
+}
