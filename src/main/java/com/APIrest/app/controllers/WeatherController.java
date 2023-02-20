231448873f0929c5fe1e5256634f4deb0b8ebdaf
LuisOT25 <delatorreluis81@gmail.com>
2023-02-19 22:07:56 -0600

    Creacion del metodo getTracks que regresa una lista de tracks de la api de Spotify

--- src/main/java/com/APIrest/app/controllers/WeatherController.java
+++ src/main/java/com/APIrest/app/controllers/WeatherController.java
@@ -1,5 +1,8 @@
 package com.APIrest.app.controllers;
 
+import com.APIrest.app.dtos.Token;
+import com.APIrest.app.services.AutenticadorSpotify;
+import com.APIrest.app.services.SpotifyService;
 import com.APIrest.app.services.WeatherService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +18,38 @@ public class WeatherController {
     private RestTemplate restTemplate;
     private WeatherService weatherService;
 
-    @Autowired
-    public WeatherController(RestTemplate restTemplate, WeatherService weatherService) {
+    private SpotifyService spotifyService;
+    private AutenticadorSpotify autenticadorSpotify;
+
+    public WeatherController(RestTemplate restTemplate, WeatherService weatherService, SpotifyService spotifyService, AutenticadorSpotify autenticadorSpotify) {
         this.restTemplate = restTemplate;
         this.weatherService = weatherService;
+        this.spotifyService = spotifyService;
+        this.autenticadorSpotify = autenticadorSpotify;
     }
-
-
-    @GetMapping(value="test", params={"city"})
+    @GetMapping(value="API", params={"city"})
     public Object getWeatherByName(@RequestParam("city") String city) throws JsonProcessingException {
-        Object temp = this.weatherService.findByName(restTemplate,city);
-        return temp;
+        double temp = this.weatherService.findByName(restTemplate,city);
+        String genero = tempToGender(temp);
+        String token = this.autenticadorSpotify.getAccessToken().getAccessToken();
+        return this.spotifyService.getTracks(genero,token);
     }
-    @GetMapping(value="test", params={"lat","lon"})
+    @GetMapping(value="API", params={"lat","lon"})
     public Object getWeatherByCoordinates(@RequestParam("lat")String latitud,
                                           @RequestParam("lon")String longitud) throws JsonProcessingException {
         Object temp = this.weatherService.findByCoordinates(restTemplate,latitud,longitud);
         return temp;
     }
+    private String tempToGender (double temp){
+        if (temp<10.0){return "classical";}
+        if (temp<15.0){return "rock";}
+        if (temp<30.0){return "pop";}
+        return "party";
+    }
+    @GetMapping("token")
 
+    public Token verToken(){
+        Token seeToken = this.autenticadorSpotify.getAccessToken();
+        return seeToken;
+    }
 }
--- src/main/java/com/APIrest/app/services/SpotifyService.java
+++ src/main/java/com/APIrest/app/services/SpotifyService.java
@@ -1,5 +1,43 @@
 package com.APIrest.app.services;
 
-public class SpotifyService {
+import com.APIrest.app.dtos.Token;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class SpotifyService {
+    @Value("${spotify.api_base_url}")
+    String spotiftyBaseUrl;
+    public List<String> getTracks (String genero, String token) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = spotiftyBaseUrl+"/search?q=gendre:"+genero+"&type=track";
+        RequestEntity<Object> request = RequestEntity
+                .method(HttpMethod.GET,url)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .body("");
+        ResponseEntity<String> response = restTemplate.exchange(request, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode items = mapper.readTree(response.getBody())
+                .path("tracks")
+                .path("items");
+        Iterator<JsonNode> it = items.elements();
+        List<String> tracks = new ArrayList<>();
+        while (it.hasNext()) {
+            JsonNode t = it.next();
+            tracks.add(t.path("name").asText());
+        }
+        return tracks;
+    }
 }
